b1e56fc70693dcbcb14a34b3537d7e69575a78a0
Prashant Deva <prashant.deva@gmail.com>
2023-09-12 19:12:37 -0700

    settings

--- lua/pdeva/set.lua
+++ lua/pdeva/set.lua
@@ -14,3 +14,12 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+
+vim.opt.scrolloff = 8
+
+vim.g.mapleader = " "
+
