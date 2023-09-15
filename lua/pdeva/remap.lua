342e849b8dd2a5f54a89e806996dda5493d41778
Prashant Deva <prashant.deva@gmail.com>
2023-09-15 10:05:24 -0700

    git shortcuts

--- lua/pdeva/remap.lua
+++ lua/pdeva/remap.lua
@@ -1,4 +1,13 @@
 vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+
+-- splits
 vim.keymap.set("n", "|", ":vsplit<CR>")
 vim.keymap.set("n", "-", ":split<CR>")
+
+-- git 
+vim.keymap.set("n", "<leader>gs", ":Git status<CR>")
+vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
+vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
+vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
+vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
 
