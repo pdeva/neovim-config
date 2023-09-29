bc6968fd3a7531fc27547ae3c5b6e52778ab62a9
Prashant Deva <prashant.deva@gmail.com>
2023-09-29 06:06:11 -0700

    init lazy

--- lua/pdeva/init.lua
+++ lua/pdeva/init.lua
@@ -1,3 +1,17 @@
+local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazyPath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazyPath,
+    })
+end
+vim.opt.rtp:prepend(lazyPath)
+
+
 require("pdeva.set")
 require("pdeva.remap")
 
