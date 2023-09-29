6219d43c24401fdc68c71e235762884f8fe34b52
Prashant Deva <prashant.deva@gmail.com>
2023-09-29 08:20:36 -0700

    scratch

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
-require("pdeva")
+local lazyPath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazyPath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazyPath,
+    })
+end
+vim.opt.rtp:prepend(lazyPath)
+require('lazy').setup('plugins')
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,54 +1,52 @@
 return {
-
-
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.3',
-        dependencies = { { 'nvim-lua/plenary.nvim' } }
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
 
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    {
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-
-    },
-    { 'nvim-treesitter/nvim-treesitter', { build = ':TSUpdate' } },
-    "nvim-lua/plenary.nvim",
-    'theprimeagen/harpoon',
-    'mbbill/undotree',
-    'tpope/vim-fugitive',
-
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            --- Uncomment these if you want to manage LSP servers from neovim
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    },
-
-    'simrat39/symbols-outline.nvim',
-
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
-    },
-
-    {
-        "SmiteshP/nvim-navic",
-        dependencies = "neovim/nvim-lspconfig"
-    },
-
-    'airblade/vim-gitgutter',
+    --{
+    --    'rose-pine/neovim',
+    --    as = 'rose-pine',
+    --    config = function()
+    --        vim.cmd('colorscheme rose-pine')
+    --    end
+    --
+    --},
+    --{ 'nvim-treesitter/nvim-treesitter', { build = ':TSUpdate' } },
+    --"nvim-lua/plenary.nvim",
+    --'theprimeagen/harpoon',
+    --'mbbill/undotree',
+    --'tpope/vim-fugitive',
+    --
+    --{
+    --    'VonHeikemen/lsp-zero.nvim',
+    --    branch = 'v3.x',
+    --    dependencies = {
+    --        --- Uncomment these if you want to manage LSP servers from neovim
+    --        { 'williamboman/mason.nvim' },
+    --        { 'williamboman/mason-lspconfig.nvim' },
+    --
+    --        -- LSP Support
+    --        { 'neovim/nvim-lspconfig' },
+    --        -- Autocompletion
+    --        { 'hrsh7th/nvim-cmp' },
+    --        { 'hrsh7th/cmp-nvim-lsp' },
+    --        { 'L3MON4D3/LuaSnip' },
+    --    }
+    --},
+    --
+    --'simrat39/symbols-outline.nvim',
+    --
+    --{
+    --    'nvim-lualine/lualine.nvim',
+    --    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
+    --},
+    --
+    --{
+    --    "SmiteshP/nvim-navic",
+    --    dependencies = "neovim/nvim-lspconfig"
+    --},
+    --
+    --'airblade/vim-gitgutter',
 }
