27921c4d599eced38decc22688faa3864dee7a32
Prashant Deva <prashant.deva@gmail.com>
2023-10-04 00:09:43 -0700

    telescope fzf

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -5,9 +5,15 @@ return {
     'airblade/vim-gitgutter',
     {
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                cond = function() return vim.fn.executable "make" == 1 end,
+            },
+        }
     },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     { 'rose-pine/neovim', name = 'rose-pine' },
     { 'nvim-treesitter/nvim-treesitter',  build = ':TSUpdate'  },
 }
